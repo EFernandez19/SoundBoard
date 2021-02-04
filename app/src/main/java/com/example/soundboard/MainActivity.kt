72f9a7cc5da9bc4f16931e3c915b7000d87d8678
Eric Fernandez <eric.ronaldo1997@gmail.com>
2021-02-03 22:12:26 -0500

    Moved buttons to default_sounds_layout. Added Drawer to activity_main.

--- app/src/main/java/com/example/soundboard/MainActivity.kt
+++ app/src/main/java/com/example/soundboard/MainActivity.kt
@@ -3,22 +3,42 @@ package com.example.soundboard
 import android.media.AudioAttributes
 import android.media.SoundPool
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.soundboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var soundpool: SoundPool? = null
-    private var sound1=0; private var sound2=0; private var sound3=0
+    private var sound1 = 0
+    private var sound2 = 0
+    private var sound3 = 0
+    private lateinit var drawer: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonSound1.setOnClickListener{buttonPressed(binding.buttonSound1.id)}
-        binding.buttonSound2.setOnClickListener{buttonPressed(binding.buttonSound2.id)}
-        binding.buttonSound3.setOnClickListener{buttonPressed(binding.buttonSound3.id)}
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+
+        drawer = binding.drawerLayout
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+
+        binding.incLayout.buttonSound1.setOnClickListener{buttonPressed(binding.incLayout.buttonSound1.id)}
+        binding.incLayout.buttonSound2.setOnClickListener{buttonPressed(binding.incLayout.buttonSound2.id)}
+        binding.incLayout.buttonSound3.setOnClickListener{buttonPressed(binding.incLayout.buttonSound3.id)}
 
         val audioattributes = AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
             .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
@@ -28,10 +48,18 @@ class MainActivity : AppCompatActivity() {
         sound1 = soundpool?.load(this, R.raw.bellsound, 1) ?:0
         sound2 = soundpool?.load(this, R.raw.applausesound, 1) ?:0
         sound3 = soundpool?.load(this, R.raw.airhornsound, 1) ?:0
+
     }
 
-    private fun buttonPressed(buttonId:Int){
-        val soundId = when(buttonId){
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else
+            super.onBackPressed()
+    }
+
+    private fun buttonPressed(buttonId: Int) {
+        val soundId = when (buttonId) {
             R.id.button_sound1 -> sound1
             R.id.button_sound2 -> sound2
             R.id.button_sound3 -> sound3
@@ -40,7 +68,7 @@ class MainActivity : AppCompatActivity() {
         soundpool?.play(soundId, 1F, 1F, 0, 0, 1F)
     }
 
-    override fun onDestroy(){
+    override fun onDestroy() {
         super.onDestroy()
         soundpool = null
     }
