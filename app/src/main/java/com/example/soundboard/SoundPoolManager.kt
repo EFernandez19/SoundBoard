37c7b31bfff6af085a57c18d177b5c97d1ae3843
Eric Fernandez <eric.ronaldo1997@gmail.com>
2021-02-23 21:03:37 -0500

    Created SoundPoolManager

---
+++ app/src/main/java/com/example/soundboard/SoundPoolManager.kt
@@ -0,0 +1,44 @@
+package com.example.soundboard
+
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.SoundPool
+
+/*
+SoundPoolManager
+will create/destroy soundpool
+load/play/stop sounds
+ */
+class SoundPoolManager(
+    var context: Context,
+    private var soundPool: SoundPool? = null,
+    private var audioAttributes: AudioAttributes? = null,
+    private val soundMap:HashMap<Int, Int> = HashMap<Int, Int>()
+) {
+
+    //instantiate the SoundPool
+    fun buildSoundPool(soundPool:SoundPool?=null, audioAttributes:AudioAttributes?=null, maxstreams: Int=5){
+        //build audioAttributes
+        this.audioAttributes = audioAttributes ?: AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+        //build soundPool
+        this.soundPool = soundPool ?: SoundPool.Builder().setMaxStreams(maxstreams).setAudioAttributes(audioAttributes)
+            .build()
+    }
+
+    //adds sounds to soundMap
+    fun addSound(soundId:Int, sound:Int){
+        soundMap[soundId] = soundPool?.load(context, sound, 1)?:0
+    }
+
+    fun playSound(soundId:Int){
+        soundMap[soundId]?.let { soundPool?.play(it, 1F, 1F, 0,0,1F) }
+    }
+
+    //destroy soundPool
+    fun destroy(){
+        soundPool=null
+        audioAttributes=null
+    }
+}
